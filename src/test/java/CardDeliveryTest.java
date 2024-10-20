e4f28cfb417dcddb3a79612acb90eacbb80a25e3
Pavel MSK <sonev@bk.ru>
2024-10-20 11:32:20 +0300

    add tests

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -18,16 +18,16 @@ public class CardDeliveryTest {
     void shouldCardDeliveryTest() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Махачкала");
-        String planinngDate = generateDate();
+        String planingDate = generateDate();
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id=date] input").setValue(planinngDate);
+        $("[data-test-id=date] input").setValue(planingDate);
         $("[data-test-id=name] input").setValue("Петров-Иванов Тимофей");
         $("[data-test-id=phone] input").setValue("+79521361515");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + planinngDate));
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planingDate));
 
     }
 
